b486609ae89ddfbbf2627678aa8b5c1941b7c83e
Debabrata Nayak <debnayak@publicisgroupe.net>
2020-03-31 19:29:09 +0530

    Initial setup

---
+++ src/index.js
@@ -0,0 +1,6 @@
+function component(){
+    const element = document.createElement('div');
+    element.innerHTML = _.join(['Hello', 'Webpack'], ' ');
+    return element;
+}
+document.body.appendChild(component());
